8c986b349167cc1574d2148a28403aaf21893445
lio-6 <leo.anger05@gmail.com>
2025-10-26 19:20:39 +0000

    added draw for one char

--- solsystem.py
+++ solsystem.py
@@ -1,10 +1,16 @@
 import math
 import time
+import curses
 
 
 """Constants"""
 G = 1  # simplified gravitational constant for easier numbers
 
+"""graphics stuff"""
+stdscr = curses.initscr() # init the screen
+height, width = stdscr.getmaxyx() # get terminal height and width 
+zoom = 1 
+camera_y, camera_x = height/2, width/2 #centers camera
 
 class CelestialBody:
     def __init__(self, name, mass, x, y, vx, vy, color, r):
@@ -58,37 +64,7 @@ def update_positions(bodies, dt):
         body.y += body.vy * dt
 
 
-def print_positions(bodies, t):
-    print(f"\nTime: {t:.2f}")
+def draw(bodies, stdscr, camera_x, camera_y, zoom, width, height):
     for body in bodies:
-        print(f"{body.name:10s} x={body.x:10.3f}, y={body.y:10.3f}")
-
-
-def main():
-    sun = CelestialBody(
-        "Sun", mass=1000, x=0, y=0, vx=0, vy=0, color="yellow", r=3
-    )
-    planet1 = CelestialBody(
-        "Planet1", mass=1, x=50, y=0, vx=0, vy=3.5, color="blue", r=1
-    )
-    planet2 = CelestialBody(
-        "Planet2", mass=2, x=100, y=0, vx=0, vy=2.5, color="green", r=1.5
-    )
-
-    bodies = [sun, planet1, planet2]
-
-    dt = 0.05  # timestep
-    t = 0
-
-    try:
-        while True:
-            update_forces(bodies)
-            update_positions(bodies, dt)
-            print_positions(bodies, t)
-            t += dt
-            time.sleep(0.05)
-    except KeyboardInterrupt:
-        print("\nSimulation stopped.")
-
-
-main()
+        screen_x = (body.x - camera_x) * zoom + width
+        screen_y = (body.y - camera_y) * zoom + height
